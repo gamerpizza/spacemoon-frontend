978661477b76dff24dc8fdf1377b1e338c4ee1b4
thecryptomaster <93931831+ivansergeev990@users.noreply.github.com>
2022-11-28 23:55:34 -0300

    Change UI for Account info page and new product page

---
+++ components/Profile/NewProductAdd.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import sourceImage from '../../public/images/default-image.jpg';
+import {AiOutlinePlus} from 'react-icons/ai';
+import { BiPencil } from "react-icons/bi";
+import startIcon from '../../public/images/star.svg'
+
+const NewProductAdd = (props:any) => {
+  return (
+    <div className='p-16 w-full'>
+      <p className='font-unica text-[30px] py-5'>LIST NEW PRODUCT</p>
+      <div className='max-w-[900px] font-comfortaa'>
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Use 3-50 characters" required />
+        </div>
+        <div className='mt-12'>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+          <textarea className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-[160px]" placeholder="Write a description of your product, max 200 characters" required />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewProductAdd;
