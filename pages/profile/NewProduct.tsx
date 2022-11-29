e6f4182d177a653b73247a735aed3123c7428906
thecryptomaster <93931831+ivansergeev990@users.noreply.github.com>
2022-11-28 23:47:26 -0300

    add new product page

---
+++ pages/profile/NewProduct.tsx
@@ -0,0 +1,15 @@
+import { useSession } from 'next-auth/react';
+import ProfileSideBar from '../../components/Profile/ProfileSideBar';
+import AccountInfo from '../../components/Profile/AccountInfo';
+
+const NewProduct = () => {
+  const session = useSession();
+  return (
+    <div className='mx-auto mt-0 md:pl-16 sm:pl-8 sm:flex min-h-[calc(100vh-260px)]'>
+      <ProfileSideBar />
+      <AccountInfo />
+    </div>
+  );
+};
+
+export default NewProduct;
