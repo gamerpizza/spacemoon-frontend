c0a58f4c398478339b5b714fcd7ddf811177f626
usmansiddique123 <usman.siddique@devsinc.com>
2022-12-02 20:50:19 +0500

    removed imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import LandingPage from "../components/LandingPage/LandingPage";
-
-import { GooglePay } from "../components/GooglePay/GooglePay";
-import { PayPal } from "../components/PayPal/PayPal";
-import { Stripe } from "../components/Stripe/Stripe";
 import Wrapper from "../components/Wrapper/Wrapper";
-import { Category } from "../model/category";
-import { mockCart, mockOrder } from "../utils/mock";
-import { Footer } from "../components/Footer/Footer";
 
 const Home: NextPage = (props: any) => (
   <Wrapper>
