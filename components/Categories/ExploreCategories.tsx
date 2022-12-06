d1563811e359ec8e2b615e4640da1c3fc833df21
usmansiddique123 <usman.siddique@devsinc.com>
2022-12-06 16:05:08 +0500

    Margins and paddings refined on landing page

--- components/Categories/ExploreCategories.tsx
+++ components/Categories/ExploreCategories.tsx
@@ -5,14 +5,14 @@ import arrowIcon from "../../public/images/right-arrow.png";
 const ExploreCategories = () => {
   return (
     <>
-      <div className="m-auto w-[90%]">
+      <div className="m-auto">
         <div className="flex justify-center">
-          <h1 className="text-4xl  pt-16 font-unica w-[80%] m-auto ">
+          <h1 className="text-4xl  pt-16 font-unica w-[99%] ">
             EXPLORE THE PRODUCTS BY CATEGORIES{" "}
           </h1>
         </div>
         <div className="flex flex-row flex-wrap justify-center ">
-          <div className="bg-[url(/images/multiverse.png)] bg-no-repeat w-[1210px] bg-contain h-[681px] mt-14 p-8">
+          <div className="bg-[url(/images/multiverse.png)] bg-no-repeat w-[500px] bg-contain h-[580px] mt-14 p-8">
             <h1 className="text-white text-2xl font-unica"> MULTIVERSE</h1>
             <button className="text-white font-thin  mt-4 pt-3  rounded-lg ">
               <div className="flex">
@@ -27,9 +27,9 @@ const ExploreCategories = () => {
               </div>
             </button>
           </div>
-          <div className=" flex flex-wrap lg:p-10">
+          <div className=" flex flex-wrap items-center pt-12 justify-center">
             <div className="p-4">
-              <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[260px] w-[500px] p-8">
+              <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[260px] w-[500px] p-8 mb-10">
                 <h1 className="text-white text-2xl font-unica">AWARA</h1>
                 <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
                   <div className="flex">
@@ -61,8 +61,8 @@ const ExploreCategories = () => {
                 </button>
               </div>
             </div>
-            <div className="p-4">
-              <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[260px] w-[500px] p-8">
+            <div className="p-4 pt-6">
+              <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[260px] w-[500px] p-8 mb-10">
                 <h1 className="text-white text-2xl font-unica">ASTRO</h1>
                 <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
                   <div className="flex">
--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 
-import arrowIcon from "../../public/images/right-arrow.png";
 import startIcon from "../../public/images/star.svg";
 import addIcon from "../../public/images/add.svg";
+import ExploreCategories from "../Categories/ExploreCategories";
 
 const LandingPage = (props: any) => {
   const router = useRouter();
@@ -25,110 +25,18 @@ const LandingPage = (props: any) => {
           </div>
         </div>
         <div className="flex flex-col h-auto justify-center">
-          <div className="m-auto w-[90%]">
-            <div className="flex justify-center">
-              <h1 className="text-4xl  pt-16 font-unica w-[80%] m-auto ">
-                {" "}
-                EXPLORE THE PRODUCTS BY CATEGORIES{" "}
-              </h1>
-            </div>
-            <div className="flex flex-row flex-wrap justify-center ">
-              <div className="bg-[url(/images/multiverse.png)] bg-no-repeat w-96 bg-contain h-[30rem] mt-14 p-8">
-                <h1 className="text-white text-2xl font-unica"> MULTIVERSE</h1>
-                <button className="text-white font-thin  mt-4 pt-3  rounded-lg ">
-                  <div className="flex">
-                    <p> Explore the best deals </p>
-                    <Image
-                      src={arrowIcon}
-                      height={20}
-                      width={20}
-                      className="ml-2"
-                      alt="arrowIcon"
-                    ></Image>
-                  </div>
-                </button>
-              </div>
-              <div className=" flex flex-wrap lg:p-10">
-                <div className="p-4">
-                  <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[15rem] w-96 p-8">
-                    <h1 className="text-white text-2xl font-unica">AWARA</h1>
-                    <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
-                      <div className="flex">
-                        <p>Check it out</p>
-                        <Image
-                          src={arrowIcon}
-                          height={20}
-                          width={20}
-                          className="ml-2"
-                          alt="arrowIcon"
-                        ></Image>
-                      </div>
-                    </button>
-                  </div>
-
-                  <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[15rem] w-96 p-8">
-                    <h1 className="text-white text-2xl font-unica">LU</h1>
-                    <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
-                      <div className="flex">
-                        <p>Check it out</p>
-                        <Image
-                          src={arrowIcon}
-                          height={20}
-                          width={20}
-                          className="ml-2"
-                          alt="arrowIcon"
-                        ></Image>
-                      </div>
-                    </button>
-                  </div>
-                </div>
-                <div className="p-4">
-                  <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[15rem] w-96 p-8">
-                    <h1 className="text-white text-2xl font-unica">ASTRO</h1>
-                    <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
-                      <div className="flex">
-                        <p>Check it out</p>
-                        <Image
-                          src={arrowIcon}
-                          height={20}
-                          width={20}
-                          className="ml-2"
-                          alt="arrowIcon"
-                        ></Image>
-                      </div>
-                    </button>
-                  </div>
-
-                  <div className="bg-[url(/images/magia.png)] bg-no-repeat bg-contain h-[15rem] w-96 p-8">
-                    <h1 className="text-white text-2xl font-unica">MAGNA</h1>
-                    <button className="text-white font-thin border-white bg-gradient-to-r from-[rgba(255,255,255,0.4)] to-[rgba(255,255,255,0.1)] mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg ">
-                      <div className="flex">
-                        <p>Check it out</p>
-                        <Image
-                          src={arrowIcon}
-                          height={20}
-                          width={20}
-                          className="ml-2"
-                          alt="arrowIcon"
-                        ></Image>
-                      </div>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ExploreCategories />
         </div>
 
         <div className="flex flex-wrap sm:justify-center 2xl:justify-between mt-10">
-          <div className="m-auto w-[90%]">
+          <div className="m-auto">
             <div className="flex justify-center">
-              <h1 className="text-3xl mb-4 font-unica w-[80%] m-auto">
+              <h1 className="text-3xl mb-4 font-unica w-[95%] m-auto">
                 STARNIGHT
               </h1>
               <br />
             </div>
-            <div className="flex flex-wrap gap-x-12 justify-center ">
+            <div className="flex flex-wrap gap-x-6 justify-center ">
               <div className="h-[380px]">
                 <div className="bg-[url(/images/default-image.jpg)] bg-no-repeat bg-cover bg-center h-[260px] w-[360px] items-end">
                   <div className="flex justify-between h-[90%] mr-2 ml-2 items-end">
@@ -245,14 +153,14 @@ const LandingPage = (props: any) => {
         </div>
 
         <div className="flex flex-wrap sm:justify-center 2xl:justify-between mt-20">
-          <div className="m-auto w-[90%]">
+          <div className="m-auto">
             <div className="flex justify-center">
-              <h1 className="text-3xl mb-4 font-unica w-[80%] m-auto">
+              <h1 className="text-3xl mb-4 font-unica w-[95%] m-auto">
                 STARNIGHT
               </h1>
               <br />
             </div>
-            <div className="flex flex-wrap gap-x-12 justify-center ">
+            <div className="flex flex-wrap gap-x-6 justify-center ">
               <div className="h-[380px]">
                 <div className="bg-[url(/images/default-image.jpg)] bg-no-repeat bg-cover bg-center h-[260px] w-[360px] items-end">
                   <div className="flex justify-between h-[90%] mr-2 ml-2 items-end">
@@ -369,14 +277,14 @@ const LandingPage = (props: any) => {
         </div>
 
         <div className="flex flex-wrap sm:justify-center 2xl:justify-between mt-20 mb-20">
-          <div className="m-auto w-[90%]">
+          <div className="m-auto ">
             <div className="flex justify-center">
-              <h1 className="text-3xl mb-4 font-unica w-[80%] m-auto">
+              <h1 className="text-3xl mb-4 font-unica w-[95%] m-auto">
                 STARNIGHT
               </h1>
               <br />
             </div>
-            <div className="flex flex-wrap gap-x-12 justify-center ">
+            <div className="flex flex-wrap gap-x-6 justify-center ">
               <div className="h-[380px]">
                 <div className="bg-[url(/images/default-image.jpg)] bg-no-repeat bg-cover bg-center h-[260px] w-[360px] items-end">
                   <div className="flex justify-between h-[90%] mr-2 ml-2 items-end">
