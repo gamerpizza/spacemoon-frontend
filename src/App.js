aaed89412cd57e75f2fee4a2db59dd2065ec963d
rogelioConsejo <rogelio.consejo@gmail.com>
2022-12-15 04:37:05 +0100

    feat: basic login modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
 import Header from './header/Header';
 import Dashboard from "./dashboard/Dashboard";
+import {useCallback, useEffect, useState} from "react";
+import * as PropTypes from "prop-types";
+
+function LoginForm({onClose}) {
+    return (
+        <div className="LoginModal">
+            <form>
+                <button className="CloseButton" onClick={onClose}>X</button>
+                <label htmlFor="UserName">User Name: </label><input type="text" name="UserName" id="UserName"/>
+                <label htmlFor="Password">Password: </label><input type="password" name="Password" id="Password"/>
+            </form>
+        </div>
+    );
+}
+
+function Login({shown, closeFunction}) {
+    return (
+        <>{shown?<LoginForm onClose={closeFunction}/>:<></>}</>
+    );
+}
+
+Login.propTypes = {shown: PropTypes.bool};
 
 function App() {
+    const [user, setUser] = useState('');
+    const [loginIsShown, setLoginIsShown] = useState(false);
+
+    function toggleLogin (){
+        setLoginIsShown(!loginIsShown);
+    }
+
     return (
         <div className="App">
-            <Header/>
+            <Login shown={loginIsShown} closeFunction={toggleLogin}/>
+            <Header user={user} handleLogin={toggleLogin}/>
             <Dashboard/>
         </div>
     );
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,15 +1,23 @@
 import './Header.css'
 import {SearchBar} from "./SearchBar";
+import * as PropTypes from "prop-types";
 
-function Header() {
+function Header({user = "", handleLogin = ()=>{}}) {
+    const toggleLogin = () => {
+        console.log("CLICK")
+        console.log(handleLogin)
+        handleLogin()
+    }
     return <header className="Header">
         <div className="Container">
             <p>SPACEMOON</p>
             <SearchBar/>
-            <p>User Menu</p>
+            {user?<p>User Menu</p>:<div><button className="LoginButton" onClick={toggleLogin}>Login</button></div>}
         </div>
 
     </header>;
 }
 
 export default Header;
+
+Header.propTypes = {user: PropTypes.string, handleLogin: PropTypes.func};
