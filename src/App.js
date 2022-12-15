c2bd43b95a41f5ecc6ca7a95095a9a972fef4e92
rogelioConsejo <rogelio.consejo@gmail.com>
2022-12-15 06:44:45 +0100

    feat: consume login API

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import {Login} from "./login/Login";
 
 function App() {
     const [user, setUser] = useState('');
+    const [token, setToken] = useState('');
     const [loginIsShown, setLoginIsShown] = useState(false);
 
     function toggleLogin (){
         setLoginIsShown(!loginIsShown);
     }
 
+    function getUserCredential(user, token){
+        console.log(user+":"+token)
+        setUser(user)
+        setToken(token)
+    }
+
     return (
         <div className="App">
-            <Login shown={loginIsShown} closeFunction={toggleLogin}/>
+            <Login shown={loginIsShown} closeFunction={toggleLogin} onLogin={getUserCredential}/>
             <Header user={user} handleLogin={toggleLogin}/>
             <Dashboard/>
         </div>
--- src/login/Login.js
+++ src/login/Login.js
@@ -2,26 +2,48 @@ import "./Login.css"
 import * as PropTypes from "prop-types";
 import {useState} from "react";
 
-function LoginForm({onClose}) {
+function LoginForm({onClose, onLoggedIn}) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [userInputIsWrong, setUserInputIsWrong] = useState(false)
     const [passInputIsWrong, setPassInputIsWrong] = useState(false)
     const tryToLogin = () => {
-        validateUser();
-        validatePassword();
+        let stop = false
+        stop = validateUser();
+        if (stop) return;
+        stop = validatePassword();
+        if (stop) return;
+
+        fetch('http://34.172.187.156:1234/login', {
+            method: "GET",
+            headers: {"Authorization": "Basic " + window.btoa(username + ":" + password)}
+        })
+            .then(response => {
+                return response.text()
+            }).then(r => {
+            let token = r.replace("token: ", "")
+            if (token.trim() !== ""){
+                onLoggedIn(username, token)
+            }
+        });
+
         function validateUser() {
             if (username.trim() === "") {
                 setUserInputIsWrong(true)
+                return true
             } else {
                 setUserInputIsWrong(false)
+                return false
             }
         }
+
         function validatePassword() {
             if (password.trim() === "") {
                 setPassInputIsWrong(true)
+                return true
             } else {
                 setPassInputIsWrong(false)
+                return false
             }
         }
     }
@@ -32,19 +54,23 @@ function LoginForm({onClose}) {
                 <button type="button" className="CloseButton" onClick={onClose}>X</button>
                 <label htmlFor="UserName">User Name: </label>
                 <input value={username} type="text" name="UserName" id="UserName" placeholder="Your Username"
-                       onChange={(event)=>{setUsername(event.target.value)}} className={userInputIsWrong?"WrongInput":""}/>
+                       onChange={(event) => {
+                           setUsername(event.target.value)
+                       }} className={userInputIsWrong ? "WrongInput" : ""}/>
                 <label htmlFor="Password">Password: </label>
                 <input type="password" name="Password" id="Password" placeholder="*****"
-                       onChange={(event)=>{setPassword(event.target.value)}} className={passInputIsWrong?"WrongInput":""}/>
+                       onChange={(event) => {
+                           setPassword(event.target.value)
+                       }} className={passInputIsWrong ? "WrongInput" : ""}/>
                 <button type="button" className="Button LoginSendButton" onClick={tryToLogin}>Login</button>
             </form>
         </div>
     );
 }
 
-export function Login({shown, closeFunction}) {
+export function Login({shown, closeFunction, onLogin}) {
     return (
-        <>{shown ? <LoginForm onClose={closeFunction}/> : <></>}</>
+        <>{shown ? <LoginForm onClose={closeFunction} onLoggedIn={onLogin}/> : <></>}</>
     );
 }
 
