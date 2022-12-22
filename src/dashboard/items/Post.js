9d7888fc404f8cf8e302e438d075e3abb7055168
rogelioConsejo <rogelio.consejo@gmail.com>
2022-12-22 23:59:59 +0100

    changed like to ✨

--- src/dashboard/items/Post.js
+++ src/dashboard/items/Post.js
@@ -25,7 +25,7 @@ function PostButtons(props) {
     return <>
          <div className={"PostButtons"}>
             <button className={"PostLikeButton"} type={"button"}
-                    onClick={props.onClick}>{props.liked ? <>🚀</> : <>🌑</>}</button>
+                    onClick={props.onClick}>{props.liked ? <>✨</> : <>🌑</>}</button>
         </div>
     </>
 }
