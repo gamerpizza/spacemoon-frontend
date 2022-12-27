cf398a2044d31bfbc694c38d4f90fdc140fe87e2
rogelioConsejo <rogelio.consejo@gmail.com>
2022-12-27 02:16:26 +0100

    feat: remove side menu

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,19 +1,8 @@
 import {Items} from "./items/Items";
 
-function SideMenu() {
-    return <aside className="Aside">
-        <ul>
-            <li>
-                <button
-                className={"AsideButtonHome"}>Home</button>
-            </li>
-        </ul>
-    </aside>;
-}
 
 function Dashboard({filterString = "", userToken="", userName=""}) {
     return <div className="Dashboard">
-            <SideMenu/>
             <Items filterString={filterString} userToken={userToken} userName={userName}/>
     </div>;
 }
--- src/dashboard/items/PostList.js
+++ src/dashboard/items/PostList.js
@@ -2,12 +2,12 @@ import "./PostList.css"
 import * as PropTypes from "prop-types";
 import {Post} from "./Post";
 
-export function PostList({items = {}, filterString = "", userToken= "", userName=""}) {
+export function PostList({items = {}, filterString = "", userToken= "", userName="", sortCompareFunc = CompareByDateDescending}) {
     const hide = items === {} || items === null || items === undefined
     return <ul className="Items PostList">
         {!hide?Object.entries(items)
             .filter(filterByString)
-            .sort(CompareByDateDescending)
+            .sort(sortCompareFunc)
             .map(([k, item]) => {
                 return <Post item={item} key={item.id} userToken={userToken} userName={userName}/>
             }):<></>
@@ -18,15 +18,14 @@ export function PostList({items = {}, filterString = "", userToken= "", userName
         return item.author.toLowerCase().includes(filterString.toLowerCase())
             || item.caption.toLowerCase().includes(filterString.toLowerCase())
     }
+}
 
-    function CompareByDateDescending([k1, item1], [k2, item2]) {
-        let createdA = Date.parse(item1.created);
-        let createdB = Date.parse(item2.created);
-        if (createdA > createdB) return -1
-        if (createdA < createdB) return 1
-        return 0
-    }
-
+function CompareByDateDescending([k1, item1], [k2, item2]) {
+    let createdA = Date.parse(item1.created);
+    let createdB = Date.parse(item2.created);
+    if (createdA > createdB) return -1
+    if (createdA < createdB) return 1
+    return 0
 }
 
 PostList.propTypes = {
